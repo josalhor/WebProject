40d0609d77b373f511baa0bcad4563e185426e67
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2020-02-10 10:31:49 +0100

    hello world

---
+++ personal_website/apps/hello_world/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+#from .views import hello_world
+from apps.hello_world.views import hello_world
+
+urlpatterns = [
+    path('', hello_world, name='hello_world_name')
+]
