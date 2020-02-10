4fb8adccd85a0728e5a67579c6c73ee30d202089
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2020-02-10 11:18:05 +0100

    hello world fix templates

--- personal_website/personal_website/settings.py
+++ personal_website/personal_website/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'personal_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'personal_website/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
